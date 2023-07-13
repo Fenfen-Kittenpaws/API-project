9bf9f8e03feb94c6c31057dcd3f457db56deebbc
Jovan <mister.jovan@gmail.com>
2023-07-12 19:49:44 -0700

    commiting changes

--- backend/db/seeders/20230713002318-demo-review.js
+++ backend/db/seeders/20230713002318-demo-review.js
@@ -1,18 +1,18 @@
 'use strict';
+const bcrypt = require("bcryptjs");
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  p: async (queryInterface, Sequelize) => {
+    options.tableName = 'Reviews';
+    return queryInterface.bulkInsert(options, [
+
+  ], {});
+},
 
   async down (queryInterface, Sequelize) {
     /**
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { Spot } = require('../db/models');
+const { restoreUser } = require('../../utils/auth');
 
 const router = express.Router();
 
+// get all spots
 router.get('/', (async (req, res) => {
     const spots = await Spot.findAll();
     return res.json(spots);
 }));
+
+//get all spots owned by current user
+
 
 module.exports = router;
