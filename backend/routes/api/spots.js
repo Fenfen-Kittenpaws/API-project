9934d52e1fcbbdd4281e5aabe6ba724026f70d5f
Jovan <mister.jovan@gmail.com>
2023-07-13 20:40:15 -0700

    creating Spot features

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const { Spot, Review, SpotImage } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth');
+const { restoreUser, requireAuth } = require('../../utils/auth');
+const { Op, Sequelize } = require('sequelize');
 
 const router = express.Router();
 
 // get all spots
-router.get('/', (async (req, res) => {
-    const spots = await Spot.findAll({
-        include: [
-            {
-                model: Review,
-                attributes: ['id', 'stars'], // Add any other Review attributes you need.
-            },
-            {
-                model: SpotImage,
-                attributes: ['id', 'url'], // Add any other SpotImage attributes you need.
-            }
-        ]
-    });
-    return res.json(spots);
-}));
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll();
 
-//get all spots owned by current user
+    return res.json(spots);
+});
+
+
 
 
 module.exports = router;
