b78a25ea19fd964a7fd67303fe19975c5120199c
Jovan <mister.jovan@gmail.com>
2023-07-16 19:57:16 -0700

    created bookingsRouter

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { Spot, Review, SpotImage, User, ReviewImage, Booking } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth');
+
+const router = express.Router();
+
+
+
+module.exports = router;
