81bfa83d22739435b6e20b144fcc3fb1d4f928d9
Jovan <mister.jovan@gmail.com>
2023-07-18 00:46:39 -0700

    fixing error handlers

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,21 +3,24 @@ const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
-const handleValidationErrors = (req, _res, next) => {
+const handleValidationErrors = (req, res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = {};
-    validationErrors
-      .array()
-      .forEach(error => errors[error.param] = error.msg);
+    const errors = validationErrors.array().reduce((acc, error) => {
+      acc[error.param] = error.msg;
+      return acc;
+    }, {});
 
-    const err = Error("Bad request.");
-    err.errors = errors;
-    err.status = 400;
-    err.title = "Bad request.";
-    next(err);
+    return res.status(400).json({
+      message: 'Bad Request',
+      errors: {
+        credential: "Email or username is required",
+        password: "Password is required"
+      }
+    });
   }
+
   next();
 };
 
