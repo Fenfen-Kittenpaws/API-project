0f7b895a64be2811d11496594c0acdfb02407f3a
Jovan <mister.jovan@gmail.com>
2023-07-17 20:25:07 -0700

    fixed bookings seeder, created spot-images.js and added the router to api/inndex.js

--- backend/db/migrations/20230711222402-create-review.js
+++ backend/db/migrations/20230711222402-create-review.js
@@ -20,7 +20,8 @@ module.exports = {
         references: {
           model: 'Spots',
           key: 'id'
-        }
+        },
+        onDelete: "CASCADE"
       },
       userId: {
         type: Sequelize.INTEGER,
@@ -28,7 +29,7 @@ module.exports = {
         references: {
           model: 'Users',
           key: 'id'
-        }
+        },
       },
       review: {
         type: Sequelize.TEXT,
@@ -51,7 +52,7 @@ module.exports = {
     }, options);
   },
   down: async (queryInterface, Sequelize) => {
-    
+
     return queryInterface.dropTable("Reviews", options);
   }
 };
