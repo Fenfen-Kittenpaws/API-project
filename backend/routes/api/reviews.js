ce5c50e7c3fe8df06223a034e4044aafcd78abbb
Jovan <mister.jovan@gmail.com>
2023-07-18 20:16:13 -0700

    fixing reviews/current v2

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -39,39 +39,7 @@ router.get('/current', restoreUser, async (req, res) => {
     });
 
     const reviewsWithPreviewImage = reviews.map(review => {
-        const reviewDTO = {
-            id: review.id,
-            userId: review.userId,
-            spotId: review.spotId,
-            review: review.review,
-            stars: review.stars,
-            createdAt: review.createdAt,
-            updatedAt: review.updatedAt,
-            User: {
-                id: review.User.id,
-                firstName: review.User.firstName,
-                lastName: review.User.lastName
-            },
-            Spot: {
-                id: review.Spot.id,
-                ownerId: review.Spot.ownerId,
-                address: review.Spot.address,
-                city: review.Spot.city,
-                state: review.Spot.state,
-                country: review.Spot.country,
-                lat: review.Spot.lat,
-                lng: review.Spot.lng,
-                name: review.Spot.name,
-                price: review.Spot.price,
-                previewImage: review.Spot.SpotImages.length ? review.Spot.SpotImages[0].url : null
-            },
-            ReviewImages: review.ReviewImages.map(image => {
-                return {
-                    id: image.id,
-                    url: image.url
-                };
-            })
-        };
+        const reviewJSON = review.toJSON();
 
         if (reviewJSON.Spot.SpotImages && reviewJSON.Spot.SpotImages.length) {
             reviewJSON.Spot.previewImage = reviewJSON.Spot.SpotImages[0].url
@@ -82,7 +50,7 @@ router.get('/current', restoreUser, async (req, res) => {
         return reviewJSON
     })
 
-    res.json({ Reviews: reviewsWithPreviewImage });
+    res.json({ Reviews: reviewsWithPreviewImage })
 })
 
 //Add an Image to a Review based on the Review's id
